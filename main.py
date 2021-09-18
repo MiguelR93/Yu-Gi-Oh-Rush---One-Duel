e82a28551de94371c4c4e363c0acbdfe21d2b55e
MiguelR93 <miguelromerop93@gmail.com>
2021-09-17 23:38:07 -0500

    Adding of images/ to .gitignore Adding the WHITE and a code to paint the screen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame, sys
 pygame.init()
 
+# my colours
+WHITE = (255, 255, 255)
+
 windowSize = (1003, 800)
 screen = pygame.display.set_mode(windowSize)
 clock = pygame.time.Clock()
@@ -10,7 +13,10 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    pygame.display.flip()
-    clock.tick(60)
+
+    screen.fill(WHITE) # paints the screen
+
+    pygame.display.flip() # actualizes the screen
+    clock.tick(60) # frames per second
 
 pygame.quit()
